ae7eb6fa5ce41f1de73d1e1df9fb28e648c7319f
Joel Rodrigues Moreira <joel.databox@gmail.com>
2017-11-27 21:46:00 -0200

    organização de modulos

--- src/app/modules/sr-http/index.ts
+++ src/app/modules/sr-http/index.ts
@@ -1,4 +1,1 @@
-export * from "./services/sr-http.service";
-export * from "./services/sr-media-type";
-export * from "./sr-criteria";
 export * from "./sr-http.module";
--- src/app/modules/sr-http/sr-http.module.ts
+++ src/app/modules/sr-http/sr-http.module.ts
@@ -3,6 +3,10 @@ import {CommonModule} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
 import {SrHttpService} from "./services/sr-http.service";
 
+export * from "./services/sr-http.service";
+export * from "./services/sr-media-type";
+export * from "./sr-criteria";
+
 @NgModule({
   imports: [
     CommonModule,
