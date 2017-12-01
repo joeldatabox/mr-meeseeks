0381c4b66fc2154b583c62b3bcf7b7fc2c4a4532
Joel Rodrigues Moreira <joel.databox@gmail.com>
2017-12-01 20:27:24 -0200

    removido o serviço de loading por conta de bugs

--- src/app/modules/sr-view-utils/sr-view-utils.module.ts
+++ src/app/modules/sr-view-utils/sr-view-utils.module.ts
@@ -7,15 +7,13 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {SrDialogService} from "./services/dialog/sr-dialog.service";
 import {CovalentDialogsModule, CovalentLoadingModule} from "@covalent/core";
 import {SrLoadingTopbarComponent} from "./components/loading-topbar/component/sr-loading-topbar.component";
-import {SrLoadingService} from "./services/loading/sr-loading.service";
-import {SrLoadingTopbarService} from "./components/loading-topbar/service/sr-loading-topbar.service";
 
-export * from "./components/loading-topbar/service/sr-loading-topbar.service";
-export * from "./components/loading-topbar/component/sr-loading-topbar.component";
-export * from "./components/loading-topbar/model/sr-loading";
+//export * from "./components/loading-topbar/service/sr-loading-topbar.service";
+//export * from "./components/loading-topbar/component/sr-loading-topbar.component";
+//export * from "./components/loading-topbar/model/sr-loading";
 
 export * from "./services/dialog/sr-dialog.service";
-export * from "./services/loading/sr-loading.service";
+//export * from "./services/loading/sr-loading.service";
 export * from "./services/meta/sr-meta.service";
 export * from "./services/snack/sr-snack.service";
 
@@ -34,17 +32,17 @@ export * from "./services/snack/sr-snack.service";
     CovalentLoadingModule
   ],
   declarations: [
-    SrLoadingTopbarComponent
+    //SrLoadingTopbarComponent
   ],
   exports: [
-    SrLoadingTopbarComponent
+    //SrLoadingTopbarComponent
   ],
   providers: [
     SrMetaService,
     SrSnackService,
-    SrDialogService,
-    SrLoadingService,
-    SrLoadingTopbarService
+    SrDialogService
+    //SrLoadingService,
+    //SrLoadingTopbarService
   ]
 })
 export class SrViewUtilsModule {
