0a9eb13c040cfdf9a67eb8672ffa09ec6d236695
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-03-28 12:23:33 -0300

    ajustes nas mascaras

--- src/app/modules/sr-mask/sr-mask.util.ts
+++ src/app/modules/sr-mask/sr-mask.util.ts
@@ -1,24 +1,27 @@
 import emailMask from "text-mask-addons/dist/emailMask";
 import {createAutoCorrectedDatePipe} from "text-mask-addons/dist/textMaskAddons";
 
-export class SrMaskUtil {
-  static readonly email = emailMask;
+const SrMaskUtil = {
+  email: () => emailMask,
+  fone: () => {
+    return {mask: ["(", /[1-9]/, /[1-9]/, ")", " ", /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]};
+  },
+  cell: () => {
+    return {mask: ["(", /[1-9]/, /[1-9]/, ")", " ", /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]};
+  },
+  cpf: () => {
+    return {
+      mask: [/[0-9]/, /[0-9]/, /[0-9]/, ".", /[0-9]/, /[0-9]/, /[0-9]/, ".", /[0-9]/, /[0-9]/, /[0-9]/, "-", /[0-9]/, /[0-9]/]
+    };
+  },
+  date: () => {
+    return {
+      mask: [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/],
+      placeholderChar: " ",
+      keepCharPositions: false,
+      pipe: createAutoCorrectedDatePipe("dd/mm/yyyy")
+    };
+  }
+};
 
-
-  static readonly fone = {mask: ["(", /[1-9]/, /[1-9]/, ")", " ", /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]};
-
-
-  static readonly cell = {mask: ["(", /[1-9]/, /[1-9]/, ")", " ", /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]};
-
-
-  static readonly cpf = {
-    mask: [/[0-9]/, /[0-9]/, /[0-9]/, ".", /[0-9]/, /[0-9]/, /[0-9]/, ".", /[0-9]/, /[0-9]/, /[0-9]/, "-", /[0-9]/, /[0-9]/],
-  };
-
-  static readonly date = {
-    mask: [/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/],
-    placeholderChar: " ",
-    keepCharPositions: false,
-    pipe: createAutoCorrectedDatePipe("dd/mm/yyyy")
-  };
-}
+export {SrMaskUtil};
