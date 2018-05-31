d5b38093899166f66aaf4b3c5bda0e362ea6b2ef
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-05-31 15:48:06 -0300

    ajustes de modulos

--- src/app/sr-meeseeks.module.ts
+++ src/app/sr-meeseeks.module.ts
@@ -4,6 +4,10 @@ import {SrHttpModule} from "./modules/sr-http/sr-http.module";
 import {SrLocalStorageModule} from "./modules/sr-local-storage/sr-local-storage.module";
 import {SrViewUtilsModule} from "./modules/sr-view-utils/sr-view-utils.module";
 import {CommonModule} from "@angular/common";
+import {SrAutoCompleteDirectiveModule} from "./modules/sr-auto-complete-directive/sr-auto-complete-directive.module";
+import {SrLocatorServiceModule} from "./modules/sr-locator-service";
+import {SrMaskModule} from "./modules/sr-mask";
+import {SrValidatorModule} from "./modules/sr-validator";
 
 
 @NgModule({
@@ -12,13 +16,21 @@ import {CommonModule} from "@angular/common";
   ],
   imports: [
     CommonModule,
+    SrAutoCompleteDirectiveModule,
     SrHttpModule,
     SrLocalStorageModule,
+    SrLocatorServiceModule,
+    SrMaskModule,
+    SrValidatorModule,
     SrViewUtilsModule
   ],
   exports: [
+    SrAutoCompleteDirectiveModule,
     SrHttpModule,
     SrLocalStorageModule,
+    SrLocatorServiceModule,
+    SrMaskModule,
+    SrValidatorModule,
     SrViewUtilsModule
   ],
   providers: [],
