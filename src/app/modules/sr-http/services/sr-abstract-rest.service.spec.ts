f26a067e0b99f082b410630e2ed3e4309c8c2062
Joel Rodrigues Moreira <joel.databox@gmail.com>
2018-11-07 23:09:51 -0200

    ajustes de bugs

--- src/app/modules/sr-http/services/sr-abstract-rest.service.spec.ts
+++ src/app/modules/sr-http/services/sr-abstract-rest.service.spec.ts
@@ -1,15 +1,15 @@
-import {inject, TestBed} from '@angular/core/testing';
+import {inject, TestBed} from "@angular/core/testing";
 
-import {SrAbstractRestService} from './sr-abstract-rest.service';
+import {SrAbstractRestService} from "./sr-abstract-rest.service";
 
-describe('SrAbstractRestService', () => {
+describe("SrAbstractRestService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [SrAbstractRestService]
     });
   });
 
-  it('should be created', inject([SrAbstractRestService], (service: SrAbstractRestService) => {
+  it("should be created", inject([SrAbstractRestService], (service: SrAbstractRestService) => {
     expect(service).toBeTruthy();
   }));
 });
