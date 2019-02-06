a389524333dca97b038a9e8715a0adda6ba61f7b
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-02-06 18:34:57 -0200

    Adicionado metodos para facilitar a regra de negocio basica

--- src/app/modules/sr-http/services/model-service.interface.ts
+++ src/app/modules/sr-http/services/model-service.interface.ts
@@ -5,19 +5,73 @@ import {SrQuery} from "../sr-criteria";
 
 
 export interface ModelService<T extends Model> {
+  /**
+   * Save a simple record
+   * @param value -> value to be saved
+   */
   save(value: T): Observable<T>;
 
+  /**
+   * Update a simple record
+   * @param value -> value to be updated
+   */
   update(value: T): Observable<T>;
 
+  /**
+   * Find a record by id
+   * @param id -> id desired
+   */
   findById(id: any): Observable<T>;
 
+
+  /**
+   * Find a record by id fully
+   * @param id -> id desired
+   */
+  findByIdFully(id: any): Observable<T>;
+
+  /**
+   * Get a first record of database
+   */
   first(): Observable<T>;
 
+  /**
+   * Get a first record of database fully
+   */
+  firstFully(): Observable<T>;
+
+  /**
+   * Delete a simple record
+   * @param value -> record desired
+   */
   delete(value: T): Observable<T>;
 
+  /**
+   * Count total of records
+   */
   count(): Observable<number>;
 
+  /**
+   * Lists the logs in a paginated fashion
+   * @query -> criterions
+   */
   list(query?: SrQuery | string): Observable<ListResource<T>>;
 
+  /**
+   * Lists the records in a paginated fashion fully
+   * @query -> criterions
+   */
+  listFully(query?: SrQuery | string): Observable<ListResource<T>>;
+
+  /**
+   * Lists all the records
+   * @query -> criterions
+   */
   listAll(query?: SrQuery | string): Observable<ListResource<T>>;
+
+  /**
+   * Lists all the records fully
+   * @query -> criterions
+   */
+  listAllFully(query?: SrQuery | string): Observable<ListResource<T>>;
 }
