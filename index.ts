f8f962b8c7405ef4f23187a5a888ff0eb35b61e8
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-02-19 09:08:00 -0300

    export modules

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "./src/app/modules/sr-auto-complete-directive/sr-auto-complete-directive.module";
+export * from "./src/app/modules/sr-carousel/index";
 export * from "./src/app/modules/sr-http/index";
 export * from "./src/app/modules/sr-local-storage/index";
 export * from "./src/app/modules/sr-locator-service/index";
