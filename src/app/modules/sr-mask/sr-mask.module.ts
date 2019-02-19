4f566d5b3fd4ef341651a52dac6abcd0338152da
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-02-18 21:32:28 -0300

    ajustes no modulo de mascara

--- src/app/modules/sr-mask/sr-mask.module.ts
+++ src/app/modules/sr-mask/sr-mask.module.ts
@@ -1,6 +1,7 @@
-import {NgModule} from "@angular/core";
+import {ModuleWithProviders, NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {TextMaskModule} from "angular2-text-mask";
+import {SrMaskService} from "./services/sr-mask.service";
 
 export * from "./sr-mask.util";
 
@@ -16,4 +17,12 @@ export * from "./sr-mask.util";
 })
 
 export class SrMaskModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SrMaskModule,
+      providers: [
+        SrMaskService
+      ]
+    };
+  }
 }
