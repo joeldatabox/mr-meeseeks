81a77a66ad0895d790575c2794a9ae31d12e4c29
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-02-25 14:42:55 -0300

    adicionado validadores para numeros

--- src/app/modules/sr-validator/sr-validators.util.ts
+++ src/app/modules/sr-validator/sr-validators.util.ts
@@ -1,11 +1,14 @@
 import {validateCpf} from "./validators/cpf.validator";
 import {validateCellNumber} from "./validators/cell.validator";
 import {validateFoneNumber} from "./validators/fone.validator";
+import {nonNegative, positiveOnly} from "./validators/number.validator";
 
 const SrValidators = {
   cpf: validateCpf,
   cell: validateCellNumber,
   fone: validateFoneNumber,
+  nonNegative: nonNegative,
+  positiveOnly: positiveOnly
 };
 
 export {SrValidators};
---
+++ src/app/modules/sr-validator/validators/number.validator.ts
@@ -0,0 +1,44 @@
+import {AbstractControl, ValidationErrors} from "@angular/forms";
+import {isNullOrUndefined, isNumber, isString} from "../../sr-utils";
+
+
+const INVALID_RESULT_NO_NEGATIVE = {
+  "number": "Please enter a non-negative number"
+};
+
+export function nonNegative(control: AbstractControl | string | number): ValidationErrors {
+  if (isNullOrUndefined(control)) {
+    return INVALID_RESULT_NO_NEGATIVE;
+  }
+  if (getNumber(control) < 0) {
+    return INVALID_RESULT_NO_NEGATIVE;
+  }
+  return null;
+}
+
+const INVALID_RESULT_POSITIVE_ONLY = {
+  "number": "Please enter a positive number only"
+};
+
+export function positiveOnly(control: AbstractControl | string | number): ValidationErrors {
+  if (isNullOrUndefined(control)) {
+    return INVALID_RESULT_NO_NEGATIVE;
+  }
+  if (getNumber(control) <= 0) {
+    return INVALID_RESULT_NO_NEGATIVE;
+  }
+  return null;
+}
+
+
+function getNumber(control: AbstractControl | string | number): number {
+  let value: any = null;
+  if (isString(control)) {
+    value = (control as string);
+  } else if (isNumber(control)) {
+    value = control as number;
+  } else {
+    value = (control as AbstractControl).value;
+  }
+  return Number(value);
+}
