70e208c73b965cff7100c6f66d4eb60537be1201
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-02-27 08:43:56 -0300

    adicionado recurso para pathparam

--- src/app/modules/sr-http/services/model-service.interface.ts
+++ src/app/modules/sr-http/services/model-service.interface.ts
@@ -3,92 +3,89 @@ import {Observable} from "rxjs";
 import {ListResource, Model} from "../model";
 import {SrQuery} from "../sr-criteria";
 
+export declare interface PathVariable {
+  [key: string]: any;
+}
 
 export interface ModelService<T extends Model> {
   /**
    * Save a simple record
    * @param value -> value to be saved
    */
-  save(value: T): Observable<T>;
+  save(value: T, pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Update a simple record
    * @param value -> value to be updated
    */
-  update(value: T): Observable<T>;
+  update(value: T, pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Find a record by id
    * @param id -> id desired
    */
-  findById(id: any): Observable<T>;
+  findById(id: any, pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Find a list of record by id
-   * @param id -> array of id desired
+   * @param ids -> array of id desired
    */
-  findByIds(ids: Array<string | T>): Observable<Array<T>>;
+  findByIds(ids: Array<string | T>, pathVariable?: PathVariable): Observable<Array<T>>;
 
   /**
    * Find a list of record by id
-   * @param id -> array of id desired
+   * @param ids -> array of id desired
    */
-  findByIds(...ids: T[] | string[]): Observable<Array<T>>;
-
-  /**
-   * Find a list of record by id
-   * @param id -> array of id desired
-   */
-  findByIds(ids: any): Observable<Array<T>>;
+  findByIds(ids: any, pathVariable?: PathVariable): Observable<Array<T>>;
 
   /**
    * Find a record by id fully
-   * @param id -> id desired
+   * @param ids -> id desired
    */
-  findByIdFully(id: any): Observable<T>;
+  findByIdFully(ids: any, pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Get a first record of database
    */
-  first(): Observable<T>;
+  first(pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Get a first record of database fully
    */
-  firstFully(): Observable<T>;
+  firstFully(pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Delete a simple record
    * @param value -> record desired
    */
-  delete(value: T): Observable<T>;
+  delete(value: T, pathVariable?: PathVariable): Observable<T>;
 
   /**
    * Count total of records
    */
-  count(): Observable<number>;
+  count(pathVariable?: PathVariable): Observable<number>;
 
   /**
    * Lists the logs in a paginated fashion
    * @query -> criterions
    */
-  list(query?: SrQuery | string): Observable<ListResource<T>>;
+  list(query?: SrQuery | string, pathVariable?: PathVariable): Observable<ListResource<T>>;
 
   /**
    * Lists the records in a paginated fashion fully
    * @query -> criterions
    */
-  listFully(query?: SrQuery | string): Observable<ListResource<T>>;
+  listFully(query?: SrQuery | string, pathVariable?: PathVariable): Observable<ListResource<T>>;
 
   /**
    * Lists all the records
    * @query -> criterions
    */
-  listAll(query?: SrQuery | string): Observable<ListResource<T>>;
+  listAll(query?: SrQuery | string, pathVariable?: PathVariable): Observable<ListResource<T>>;
 
   /**
    * Lists all the records fully
    * @query -> criterions
    */
-  listAllFully(query?: SrQuery | string): Observable<ListResource<T>>;
+  listAllFully(query?: SrQuery | string, pathVariable?: PathVariable): Observable<ListResource<T>>;
 }
