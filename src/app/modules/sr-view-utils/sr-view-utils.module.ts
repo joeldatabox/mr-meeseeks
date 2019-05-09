9510c73b9563bcf85bd2af3e0311ff408b8ec911
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-05-09 08:40:34 -0300

    removido o service de snack

--- src/app/modules/sr-view-utils/sr-view-utils.module.ts
+++ src/app/modules/sr-view-utils/sr-view-utils.module.ts
@@ -1,7 +1,6 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {SrMetaService} from "./services/meta/sr-meta.service";
-import {SrSnackService} from "./services/snack/sr-snack.service";
 import {MatDialogModule, MatProgressBarModule, MatSnackBarModule} from "@angular/material";
 import {SrDialogService} from "./services/dialog/sr-dialog.service";
 import {CovalentDialogsModule, CovalentLoadingModule} from "@covalent/core";
@@ -15,7 +14,6 @@ export * from "./components/index";
 export * from "./services/dialog/sr-dialog.service";
 export * from "./services/loading/sr-loading.service";
 export * from "./services/meta/sr-meta.service";
-export * from "./services/snack/sr-snack.service";
 export * from "./services/view-injector/sr-view-injector.service";
 
 /**
@@ -41,7 +39,6 @@ export * from "./services/view-injector/sr-view-injector.service";
   ],
   providers: [
     SrMetaService,
-    SrSnackService,
     SrDialogService,
     SrLoadingService,
     SrLoadingTopbarService,
