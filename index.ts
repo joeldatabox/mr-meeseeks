41601a84369722485a47ace2455af5394fe5a942
Joel Rodrigues Moreira <joel.databox@gmail.com>
2019-07-16 17:31:19 -0300

    ajustes de exportação

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ export * from "./src/app/modules/sr-http/index";
 export * from "./src/app/modules/sr-local-storage/index";
 export * from "./src/app/modules/sr-locator-service/index";
 export * from "./src/app/modules/sr-mask/index";
+export * from "./src/app/modules/sr-route-utils/sr-route-utils.module";
 export * from "./src/app/modules/sr-utils/index";
 export * from "./src/app/modules/sr-validator/index";
 export * from "./src/app/modules/sr-view-utils/sr-view-utils.module";
