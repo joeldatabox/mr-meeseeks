2dd7930ce101cea21e8f8ab9097825cac04cb60b
Joel Rodrigues Moreira <joel.databox@gmail.com>
2020-01-10 16:30:05 -0300

    adicionado funções de comparação

--- src/app/modules/sr-http/model/model.ts
+++ src/app/modules/sr-http/model/model.ts
@@ -1,4 +1,12 @@
-import {isArray, isEmpty, isNullOrUndefined, isObject, isString} from "../../sr-utils/commons/sr-commons.model";
+import {
+  isArray,
+  isEmpty,
+  isEquals,
+  isNotNullOrUndefined,
+  isNullOrUndefined,
+  isObject,
+  isString
+} from "../../sr-utils/commons/sr-commons.model";
 import {ClassTransformOptions, plainToClass, plainToClassFromExist, TransformOptions} from "class-transformer";
 
 
@@ -16,66 +24,19 @@ export namespace Model {
     return new type();
   }
 
+  export function compare<T extends Model>(value: T, otherValue: T): boolean {
+    if (isNullOrUndefined(value) && isNullOrUndefined(otherValue)) {
+      return true;
+    }
+    if ((isNullOrUndefined(value) && isNotNullOrUndefined(otherValue)) || (isNullOrUndefined(otherValue) && isNotNullOrUndefined(value))) {
+      return false;
+    }
+    return value.id === otherValue.id;
+  }
 
-  /*export function databinding(value: any, model: any): object {
-    //se o value for null apenas setamos null também
-    if (isNullOrUndefined(value)) {
-      model = null;
-      return model;
-      //devemos verificar se é necessário instanciar o model
-    } else if (isNullOrUndefined(model)) {
-      //verificando qual é o timo do model
-      if (isObject(value)) {
-        model = {};
-      } else if (isString(value)) {
-        model = "";
-      } else if (isBoolean(value)) {
-        model = false;
-      } else if (isDate(value)) {
-        model = new Date();
-      } else if (isNumber(value)) {
-        model = 0;
-      }
-    }
-    //se for um array devemos percorrer todos os indices
-    if (isArray(value)) {
-      if (isNotNullOrUndefined(model)) {
-        model = [];
-      }
-      //percorrendo indices
-      (value as Array<any>).map((item, index) => {
-        //se existir o indice atual no model devemos utilizar a recursividade
-        if (isNotNullOrUndefined(model[index])) {
-          model[index] = databinding(item, model[index]);
-        } else {
-          //se não apenas atribuimos
-          model.push(item);
-        }
-      });
-    } else if (isObject(value)) {
-      //pegando todas as chaves do objeto
-      Object.keys(value).forEach(key => {
-        //pegando o campo da chave atual
-        const path = value[key];
-        //verificando apenas campos não nulos
-        if (isNotNullOrUndefined(path)) {
-          //se o campo for algum tipo primitivo, basta apenas adicionar o mesmo no retorno
-          if (isString(path) || isBoolean(path) || isDate(path) || isNumber(path)) {
-            model[key] = path;
-            //se for um object faz recursividade
-          } else if (isObject(path)) {
-            model[key] = databinding(path, model[key]);
-          } else {
-            //caso contrario deixa a porra do jeito que ta
-            model[key] = path;
-          }
-        } else {
-          model[key] = null;
-        }
-      });
-    }
-    return model;
-  }*/
+  export function simpleCompare(value: any, otherValue: any): boolean {
+    return isEquals(value, otherValue);
+  }
 
   /**
    * Realiza o processo de databinding de um determinado component
